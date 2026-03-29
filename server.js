9f256ae75e4d7cee15ed013ad78c443fe60dd0df
Mohammad Akhil <mohammadakhil6303@gmail.com>
2026-03-29 23:48:51 +0530

    clean backend without secrets

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 const passport = require("passport");
 const session = require("express-session");
 app.use(cors({
-  origin: "https://enginotes.in",
+  origin: ["https://enginotes.in",
+    "https://www.enginotes.in"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 app.use(express.json());
